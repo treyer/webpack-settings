06aa8d51b22c650a8c2e1c8702553588ca99aae0
treyer <nemereno_by@mail.ru>
2022-12-13 23:56:44 +0300

    feat: add husky, set pre-commit run lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from "react";
-
 function App() {
   return (
     <div className="wrapper">
       <div className="inner">
         <h1>Hello world!!</h1>
-        <img
-          src="./assets/svg/bird.svg"
-          alt="Bird"
-          width="50px"
-          height="30px"
-        />
+        <img src="./assets/svg/bird.svg" alt="Bird" width="50px" height="30px" />
         <img src="./assets/example.jpg" alt="Example" width="200px" />
         <img src="./assets/beer-example.jpg" alt="Beer example" width="200px" />
       </div>
